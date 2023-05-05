0b7f539baa2a03b4fedbd5a46bf6b001bd7755fb
Ben Forshey <ben@integrisweb.com>
2023-05-05 17:05:40 -0400

    feat(books): add styles, notes

---
+++ src/routes/books/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import './books.css';
+</script>
+
+<slot />
--- src/routes/books/the-pragmatic-programmer/+page.svelte
+++ src/routes/books/the-pragmatic-programmer/+page.svelte
@@ -3,8 +3,9 @@
 <p>Someday, I'll stop typing &ldquo;Pragmatic&rdquo; as <s>Pragmattic</s>!</p>
 
 <section>
-	<h2>Questions for Discussion</h2>
+	<h2>Reading Notes</h2>
 
+	<h3>Foreword / Preface</h3>
 	<dl>
 		<dt>pp. xx&ndash;xxi: What &ldquo;Pragmatic Programmer&rdquo; characteristics do you share?</dt>
 		<dd>
@@ -44,15 +45,81 @@
 			</dl>
 		</dd>
 	</dl>
+
+	<h3>Chapter One</h3>
+
+	<dl>
+		<dt>pp. 1&ndash;1: It&rsquo;s Your Life</dt>
+		<dd>Do I act as if I have agency, or do I accept what I&rsquo;m not satisfied with?</dd>
+
+		<dt>pp. 3&ndash;4: The Cat Ate My Source Code</dt>
+		<dd>
+			This is a message I need to hear now and again (though I consider myself a fairly trustworthy
+			person): take responsibility for your actions. The thought of rubber ducking your excuse is
+			particularly useful, as you'll have an opportunity to come up with options instead of lame
+			excuses.
+		</dd>
+
+		<dt>pp. 6&ndash;8: Software Entropy</dt>
+		<dd>
+			<p>
+				I&rsquo;ll admit that the &ldquo;Broken Windows&rdquo; model of policing has its detractors
+				(with good cause, I imagine), but I know in my core that I tent to treat shabby projects
+				without the care they need. I love the practicality of suggesting that even a small step
+				helps, like sending a 501 Not Implemented on that API endpoint that&rsquo;s not quite ready
+				to be written.
+			</p>
+			<p>
+				I&rsquo;d be interested in exploring the tension between Martin Fowler&rsquo;s
+				&ldquo;Litter&dash;Pickup Refactoring&rdquo; and the &ldquo;pull requests should be
+				small&rdquo; mindset. My own thoughts? I think commits should be focused, but pull requests
+				are only a burden because we don&rsquo;t pair program on features and we often don&rsquo;t
+				write comprehensive tests to cover those features.
+			</p>
+		</dd>
+
+		<dt>pp. 8&ndash;11: Stone Soup and Boiled Frogs</dt>
+		<dd>&nbsp;</dd>
+
+		<dt>pp. 11&ndash;13: Good&dash;Enough Software</dt>
+		<dd>&nbsp;</dd>
+
+		<dt>pp. 13&ndash;19: Your Knowledge Portfolio</dt>
+		<dd>&nbsp;</dd>
+
+		<dt>pp. 19&ndash;25: Communicate!</dt>
+		<dd>&nbsp;</dd>
+	</dl>
 </section>
 
 <section>
-	<h2>Tips (one&ndash;one hundred)</h2>
+	<h2>One Hundred Tips</h2>
 
 	<dl>
 		<dt>p. xvi: &numero;1 Care About Your Craft</dt>
 		<dd>Not just care about being right, or care about being respected: care about your craft.</dd>
+
 		<dt>p. xvi: &numero;2 Think! About Your Work</dt>
 		<dd>It&rsquo;s hard work but rewarding. You&rsquo;ll improve in your craftsmanship.</dd>
+
+		<dt>p. 1: &numero;3 You Have Agency</dt>
+		<dd>
+			&ldquo;You can change your organization or change your organization.&rdquo; &mdash;<cite
+				>Martin Fowler</cite
+			>
+		</dd>
+
+		<dt>p. 4: &numero;4 Provide Options, Don&rsquo;t Make Lame Excuses</dt>
+		<dd>
+			You can factor in everything that went wrong&mdash;including that which is outside your
+			control&mdash;but holding yourself accountable and providing options to move forward shows
+			humility and dedication to your craft.
+		</dd>
+
+		<dt>p. 7: &numero;5 Don&rsquo;t Live with Broken Windows</dt>
+		<dd>
+			Hey, that project you worked on where that suite of tests didn't pass but you never fixed it?
+			It&rsquo;s only getting worse&hellip;you&rsquo;ve stopped writing new tests.
+		</dd>
 	</dl>
 </section>
